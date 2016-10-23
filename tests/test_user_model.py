97fcf9328fc0088e3bafe71125ad28cffd3d7d40
leishunbo <leishunbo@gmail.com>
2016-10-23 17:33:10 +0800

    version 8e

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from app import create_app, db
 from app.models import User
 
@@ -10,7 +11,7 @@ class UserModelTestCase(unittest.TestCase):
         self.app_context.push()
         db.create_all()
 
-    def testDown(self):
+    def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
@@ -26,5 +27,34 @@ class UserModelTestCase(unittest.TestCase):
 
     def test_password_verification(self):
         u = User(password='cat')
+        self.assertTrue(u.verify_password('cat'))
+        self.assertFalse(u.verify_password('dog'))
+
+    def test_password_salts_are_random(self):
+        u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_valid_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertTrue(u.confirm(token))
+
+    def test_invalid_confirmation_token(self):
+        u1 = User(password='cat')
+        u2 = User(password='dog')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        token = u1.generate_confirmation_token()
+        self.assertFalse(u2.confirm(token))
+
+    def test_expired_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token(1)
+        time.sleep(2)
+        self.assertFalse(u.confirm(token))
