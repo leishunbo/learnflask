f8b1f8dc21dd760d41588e413416bd3bcf0e4584
leishunbo <leishunbo@gmail.com>
2016-11-20 00:17:28 +0800

    version 11d

--- config.py
+++ config.py
@@ -17,6 +17,7 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <495581940@qq.com>'
     # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_ADMIN = "leishunbo@vip.qq.com"
+    FLASKY_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
